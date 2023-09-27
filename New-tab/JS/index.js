8aea4bf1eecd11529ef939fda5fd497f212be4ce
Omarmousa72 <139303831+Omarmousa72@users.noreply.github.com>
2023-09-27 18:41:01 +0300

    Add files via upload

---
+++ New-tab/JS/index.js
@@ -0,0 +1,42 @@
+
+// Get a reference to the div element
+var youtube = document.getElementById("youtube");
+var facebook = document.getElementById("facebook");
+var google = document.getElementById("google");
+var yahoo = document.getElementById("yahoo");
+var twitch = document.getElementById("twitch");
+var gpt = document.getElementById("gpt");
+
+
+// Add a click event listener to the div
+youtube.addEventListener("click", function () {
+    // Redirect to the desired URL when the div is clicked
+    window.location.href = "https://www.youtube.com"; // Replace with your desired URL
+});
+
+facebook.addEventListener("click", function () {
+    // Redirect to the desired URL when the div is clicked
+    window.location.href = "https://www.facebook.com"; // Replace with your desired URL
+});
+
+google.addEventListener("click", function () {
+    // Redirect to the desired URL when the div is clicked
+    window.location.href = "https://www.google.com"; // Replace with your desired URL
+});
+
+yahoo.addEventListener("click", function () {
+    // Redirect to the desired URL when the div is clicked
+    window.location.href = "https://www.yahoo.com"; // Replace with your desired URL
+});
+
+twitch.addEventListener("click", function () {
+    // Redirect to the desired URL when the div is clicked
+    window.location.href = "https://www.twitch.com"; // Replace with your desired URL
+});
+
+gpt.addEventListener("click", function () {
+    // Redirect to the desired URL when the div is clicked
+    window.location.href = "https://chat.openai.com/"; // Replace with your desired URL
+});
+
+
---
+++ new-site/JS/new-site.js
@@ -0,0 +1,12 @@
+
+// Get a reference to the div element
+var logo = document.getElementById("moon-icon");
+
+
+
+// Add a click event listener to the div
+logo.addEventListener("click", function () {
+    // Redirect to the desired URL when the div is clicked
+    window.location.href = "E:/Web-Programming/sites/new-site/index.html"; // Replace with your desired URL
+});
+
