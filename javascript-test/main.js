88970635478a362706ae9d4a2edd2edbcde9d84e
anonymous-011 <139303831+Omarmousa72@users.noreply.github.com>
2023-11-24 22:21:31 +0200

    1

---
+++ javascript-test/main.js
@@ -0,0 +1,7 @@
+// Your Code Goes Here
+
+//BMI Formula (weight / (height * height)) [weight should be in kilograms & height should be in meters]
+
+// You should calculate BMI and if BMI < 18.6 should display `Under Weight: ${BMI}`
+// Else If BMI >= 18.6 && BMI < 24.9 should display `Normal: ${BMI}`
+// Else should display `Over Weight: ${BMI}`
