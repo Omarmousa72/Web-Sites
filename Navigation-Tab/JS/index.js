6c659082ed27381a0b2ac24eadcfab07a6a30a74
anonymous-011 <139303831+Omarmousa72@users.noreply.github.com>
2023-11-30 22:32:04 +0200

    Done

--- Navigation-Tab/JS/index.js
+++ Navigation-Tab/JS/index.js
@@ -1,42 +1,64 @@
 
 // Get a reference to the div element
-var youtube = document.getElementById("youtube");
-var facebook = document.getElementById("facebook");
-var google = document.getElementById("google");
-var yahoo = document.getElementById("yahoo");
-var twitch = document.getElementById("twitch");
-var gpt = document.getElementById("gpt");
+// let youtube = document.getElementById("youtube"),
+//     facebook = document.getElementById("facebook"),
+//     google = document.getElementById("google"),
+//     yahoo = document.getElementById("yahoo"),
+//     twitch = document.getElementById("twitch"),
+//     gpt = document.getElementById("gpt");
+let SearchIco = document.getElementById("search-icon"),
+    SearchBtn = document.getElementById("btn"),
+    SearchForm = document.getElementById("search-form"),
+    SearchFormInput = document.getElementById("SearchFormInput"),
+    isOpened = false;
 
 
 // Add a click event listener to the div
-youtube.addEventListener("click", function () {
-    // Redirect to the desired URL when the div is clicked
-    window.location.href = "https://www.youtube.com"; // Replace with your desired URL
-});
+// youtube.addEventListener("click", function () {
+//     // Redirect to the desired URL when the div is clicked
+//     window.location.href = "https://www.youtube.com"; // Replace with your desired URL
+// });
 
-facebook.addEventListener("click", function () {
-    // Redirect to the desired URL when the div is clicked
-    window.location.href = "https://www.facebook.com"; // Replace with your desired URL
-});
+// facebook.addEventListener("click", function () {
+//     // Redirect to the desired URL when the div is clicked
+//     window.location.href = "https://www.facebook.com"; // Replace with your desired URL
+// });
 
-google.addEventListener("click", function () {
-    // Redirect to the desired URL when the div is clicked
-    window.location.href = "https://www.google.com"; // Replace with your desired URL
-});
+// google.addEventListener("click", function () {
+//     // Redirect to the desired URL when the div is clicked
+//     window.location.href = "https://www.google.com"; // Replace with your desired URL
+// });
 
-yahoo.addEventListener("click", function () {
-    // Redirect to the desired URL when the div is clicked
-    window.location.href = "https://www.yahoo.com"; // Replace with your desired URL
-});
+// yahoo.addEventListener("click", function () {
+//     // Redirect to the desired URL when the div is clicked
+//     window.location.href = "https://www.yahoo.com"; // Replace with your desired URL
+// });
 
-twitch.addEventListener("click", function () {
-    // Redirect to the desired URL when the div is clicked
-    window.location.href = "https://www.twitch.com"; // Replace with your desired URL
-});
+// twitch.addEventListener("click", function () {
+//     // Redirect to the desired URL when the div is clicked
+//     window.location.href = "https://www.twitch.com"; // Replace with your desired URL
+// });
 
-gpt.addEventListener("click", function () {
-    // Redirect to the desired URL when the div is clicked
-    window.location.href = "https://chat.openai.com/"; // Replace with your desired URL
-});
+// gpt.addEventListener("click", function () {
+//     // Redirect to the desired URL when the div is clicked
+//     window.location.href = "https://chat.openai.com/"; // Replace with your desired URL
+// });
+
+const clicked = () => {
+    if (isOpened) {
+        SearchBtn.style.width = "50px";
+        SearchBtn.style.borderRadius = "50%";
+        SearchBtn.style.transition = "width 400ms ease";
+        SearchFormInput.value = "";
+        SearchForm.classList.remove('active');
+    } else {
+        SearchBtn.style.width = "390px";
+        SearchBtn.style.borderRadius = "50px";
+        SearchBtn.style.transition = "width 400ms ease";
+        SearchForm.classList.add("active");
+    }
+    isOpened = !isOpened;
+}
+SearchIco.onclick = clicked;
 
 
