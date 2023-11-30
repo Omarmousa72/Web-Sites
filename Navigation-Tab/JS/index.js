fd3f613aad0b96ac8840304ce29814c4d634b5c1
anonymous-011 <139303831+Omarmousa72@users.noreply.github.com>
2023-11-30 23:28:00 +0200

    Search-Box-Done

--- Navigation-Tab/JS/index.js
+++ Navigation-Tab/JS/index.js
@@ -1,12 +1,14 @@
 
 // Get a reference to the div element
-// let youtube = document.getElementById("youtube"),
-//     facebook = document.getElementById("facebook"),
-//     google = document.getElementById("google"),
-//     yahoo = document.getElementById("yahoo"),
-//     twitch = document.getElementById("twitch"),
-//     gpt = document.getElementById("gpt");
-let SearchIco = document.getElementById("search-icon"),
+let youtube = document.getElementById("youtube"),
+    facebook = document.getElementById("facebook"),
+    google = document.getElementById("google"),
+    yahoo = document.getElementById("yahoo"),
+    twitch = document.getElementById("twitch"),
+    gpt = document.getElementById("gpt"),
+    //<==========================================>
+    toggle = document.getElementById("toggle"),
+    SearchIco = document.getElementById("search-icon"),
     SearchBtn = document.getElementById("btn"),
     SearchForm = document.getElementById("search-form"),
     SearchFormInput = document.getElementById("SearchFormInput"),
@@ -47,16 +49,30 @@ let SearchIco = document.getElementById("search-icon"),
 const clicked = () => {
     if (isOpened) {
         SearchBtn.style.width = "50px";
-        SearchFormInput.value = "";
+
+        SearchIco.style.display = "none";
         SearchForm.classList.remove('active');
     } else {
-        SearchBtn.style.width = "390px";
+        SearchBtn.style.width = "420px";
         SearchBtn.style.borderRadius = "50px";
         SearchBtn.style.transition = "width 400ms ease";
+        SearchIco.style.display = "block";
         SearchForm.classList.add("active");
     }
     isOpened = !isOpened;
 }
-SearchIco.onclick = clicked;
+
+const search = () => {
+    var searchQuery = document.getElementById('SearchFormInput').value;
+    if (searchQuery === "") {
+
+    } else {
+        SearchForm.action = "https://www.google.com/search?q=" + encodeURIComponent(searchQuery);
+        SearchForm.submit();
+        SearchFormInput.value = "";
+    }
+}
+toggle.onclick = clicked;
+SearchIco.onclick = search;
 
 
